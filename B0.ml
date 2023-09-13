6cd5435cbae4293e1d85797cdf50e10c4f7b2fa2
Daniel Bünzli <daniel.buenzli@erratique.ch>
2023-09-13 23:12:27 +0200

    Track b0

--- B0.ml
+++ B0.ml
@@ -24,24 +24,24 @@ let example =
 
 let default =
   let meta =
-    let open B0_meta in
-    empty
-    |> add authors ["The htmlit programmers"]
-    |> add maintainers ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
-    |> add homepage "https://erratique.ch/software/htmlit"
-    |> add online_doc "https://erratique.ch/software/htmlit/doc"
-    |> add licenses ["ISC"]
-    |> add repo "git+https://erratique.ch/repos/htmlit.git"
-    |> add issues "https://github.com/dbuenzli/htmlit/issues"
-    |> add description_tags ["web"; "html"; "org:erratique"; ]
-    |> add B0_opam.Meta.build
+    B0_meta.empty
+    |> B0_meta.(add authors) ["The htmlit programmers"]
+    |> B0_meta.(add maintainers)
+       ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
+    |> B0_meta.(add homepage) "https://erratique.ch/software/htmlit"
+    |> B0_meta.(add online_doc) "https://erratique.ch/software/htmlit/doc"
+    |> B0_meta.(add licenses) ["ISC"]
+    |> B0_meta.(add repo) "git+https://erratique.ch/repos/htmlit.git"
+    |> B0_meta.(add issues) "https://github.com/dbuenzli/htmlit/issues"
+    |> B0_meta.(add description_tags) ["web"; "html"; "org:erratique"; ]
+    |> B0_meta.tag B0_opam.tag
+    |> B0_meta.add B0_opam.build
       {|[["ocaml" "pkg/pkg.ml" "build" "--dev-pkg" "%{dev}%"]]|}
-    |> tag B0_opam.tag
-    |> add B0_opam.Meta.depends
+    |> B0_meta.add B0_opam.depends
       [ "ocaml", {|>= "4.14.0"|};
         "ocamlfind", {|build|};
         "ocamlbuild", {|build|};
         "topkg", {|build & >= "1.0.3"|}; ]
   in
-  B0_pack.v "default" ~doc:"htmlit package" ~meta ~locked:true @@
+  B0_pack.make "default" ~doc:"htmlit package" ~meta ~locked:true @@
   B0_unit.list ()
